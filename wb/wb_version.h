604f53cbf0f2396c3b8112e618a11b54319e0824
Bear <programming@hallhome.us>
2026-08-06 13:37:47 -0500

    Update version to 2026.8.6 across build configuration and version header

--- wb/wb_version.h
+++ wb/wb_version.h
@@ -2,7 +2,7 @@
 // Current WinBinder version
 // This file is generated automatically by WinBinder Release Maker.
 
-#define WINBINDER_VERSION "2026.3.1"
+#define WINBINDER_VERSION "2026.8.6"
 #define WB_MAJ 2026
-#define WB_MIN 3
-#define WB_REL 1
+#define WB_MIN 8
+#define WB_REL 6
